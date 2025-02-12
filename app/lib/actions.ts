db503f6efe8d7c4567967d31052aacfa4b3c62f3
Zakaryae Mazouzi <KZY4JD@NAM.CORP.GM.COM>
2025-02-12 10:39:07 +0100

    Adding authentication using NextAuth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,9 @@ import { z } from 'zod';
  import postgres from 'postgres';
  import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from 'auth';
+import { AuthError } from 'next-auth';
+
 
 
  const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
@@ -111,4 +114,23 @@ export async function deleteInvoice(id: string) {
     console.error(error);
  }
  revalidatePath('dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
